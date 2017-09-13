02e08f4d13b682faa4aa6970c803955c11e73c8a
Ilia Barahovsky <barahilia@gmail.com>
2017-09-13 22:01:36 +0300

    Single file to hash as positional argument

--- spamsum/params.py
+++ spamsum/params.py
@@ -17,10 +17,13 @@ def get_params():
     parser.add_argument('-T', '--threshold', action='store', type=int,
         help='stop search db when score above threshold is found')
 
-    parser.add_argument('-c', action='store', dest='spamsum_hash',
+    parser.add_argument('-c', action='store', dest='hash_to_search',
         help='spamsum hash for db search (like: 24:AVP/6Lo8e5y:X09y)')
 
-    parser.add_argument('-C', action='store', dest='file_to_hash',
+    parser.add_argument('-C', action='store', dest='file_to_search',
+        help='path to file for hashing and db search')
+
+    parser.add_argument('file_to_hash', action='store',
         help='path to file for hashing and db search')
 
     return parser.parse_args()
