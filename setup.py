98d92e28548537f9f9630330233b476a786a6335
Ilia Barahovsky <barahilia@gmail.com>
2017-09-18 22:26:47 +0300

    Adding setup.py - WIP

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='spamspy',
+    version='0.1dev',
+    packages=['spamsum'],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
